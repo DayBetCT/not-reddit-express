51b74c2bfe7099d30b992863c80adcba0ef9a79d
Davette C. Trinidad <davette@goworkwize.com>
2022-10-14 22:01:46 +0800

    Add async await to the routes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -9,20 +9,27 @@ import { Request, Response, Router } from 'express';
 
 export const PostRoute = () => {
   return [
-    Router().get('/', (request: Request, response: Response) => {
-      indexPost(request, response);
+    Router().get('/', async (request: Request, response: Response) => {
+      await indexPost(request, response);
     }),
-    Router().get('/:postId', (request: Request, response: Response) => {
-      showPost(request, response);
+
+    Router().get('/:postId', async (request: Request, response: Response) => {
+      await showPost(request, response);
     }),
-    Router().post('/', (request: Request, response: Response) => {
-      storePost(request, response);
+
+    Router().post('/', async (request: Request, response: Response) => {
+      await storePost(request, response);
     }),
-    Router().put('/:postId', (request: Request, response: Response) => {
-      updatePost(request, response);
+
+    Router().put('/:postId', async (request: Request, response: Response) => {
+      await updatePost(request, response);
     }),
-    Router().delete('/:postId', (request: Request, response: Response) => {
-      destroyPost(request, response);
-    }),
+
+    Router().delete(
+      '/:postId',
+      async (request: Request, response: Response) => {
+        await destroyPost(request, response);
+      }
+    ),
   ];
 };
