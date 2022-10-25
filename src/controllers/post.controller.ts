2374758e48a5eeb0a27f3a723b9278183ca62239
Davette C. Trinidad <davette@goworkwize.com>
2022-10-25 22:45:26 +0800

    Make post and put functional

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,8 +1,10 @@
 import { Post } from '@models/post.model';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 export const indexPost = async (request: Request, response: Response) => {
-  const posts = await Post.findAll({ paranoid: false }).catch();
+  const posts = await Post.findAll({
+    order: [['created_at', 'DESC']],
+  });
 
   return response.status(200).json({ data: posts });
 };
@@ -17,22 +19,32 @@ export const showPost = async (request: Request, response: Response) => {
   return response.status(200).json({ data: post });
 };
 
-export const updatePost = (request: Request, response: Response) => {
-  response.status(200).json({ message: 'updatePost' });
+export const updatePost = async (request: Request, response: Response) => {
+  try {
+    await Post.update(request.body, {
+      where: { id: request.params.postId },
+    });
+  } catch (error) {
+    return response.status(404).json({ message: 'post not found' });
+  }
+
+  return response
+    .status(200)
+    .json({ message: await Post.findByPk(request.params.postId) });
 };
 
-export const storePost = (request: Request, response: Response) => {
-  response.status(200).json({ message: 'storePost' });
+export const storePost = async (request: Request, response: Response) => {
+  const post = await Post.create(request.body);
+
+  response.status(200).json({ data: post });
 };
 
 export const destroyPost = async (request: Request, response: Response) => {
-  const post = await Post.findByPk(request.params.postId);
-
-  if (!post?.get()) {
+  try {
+    await Post.destroy({ where: { id: request.params.postId } });
+  } catch (error) {
     return response.status(404).json({ message: 'post does not exist' });
   }
 
-  post.destroy();
-
-  return response.status(200).json({ data: post });
+  return response.sendStatus(204);
 };
