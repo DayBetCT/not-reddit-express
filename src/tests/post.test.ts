478ec41335aec1bf8b35d58a0dd755c1f8e2f630
Davette C. Trinidad <davette@goworkwize.com>
2022-11-05 14:13:47 +0800

    Add test for update and delete

--- src/tests/post.test.ts
+++ src/tests/post.test.ts
@@ -37,4 +37,29 @@ describe("manage posts", () => {
     expect(post).not.toBeNull();
     expect(post?.get()).toHaveProperty("text", text);
   });
+
+  it("should update a post", async () => {
+    const text = faker.lorem.sentence(5);
+
+    await supertest(app())
+      .put("/posts/20")
+      .set("Content-type", "application/json")
+      .send({ text })
+      .expect(200);
+
+    const post = await Post.findOne({ where: { text } });
+
+    expect(post).not.toBeNull();
+    expect(post?.get()).toHaveProperty("text", text);
+  });
+
+  // TODO prepare data before each test
+  // it("should delete a post", async () => {
+  //   await supertest(app()).delete("/posts/10000").expect(204);
+
+  //   const post = await Post.findOne({ where: { id: 10000 }, paranoid: false });
+
+  //   expect(post).not.toBeNull();
+  //   expect(post?.get()).toHaveProperty("deleted_at", expect.any(Date));
+  // });
 });
