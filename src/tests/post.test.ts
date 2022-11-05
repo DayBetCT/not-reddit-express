e8363162ea6829f600d0188275c41d1f377eb8b3
Davette C. Trinidad <davette@goworkwize.com>
2022-11-05 13:30:47 +0800

    Update index, show and store API tests to have expectations

--- src/tests/post.test.ts
+++ src/tests/post.test.ts
@@ -1,14 +1,39 @@
+import { Post } from "@models/post.model";
 import { app } from "@src/app";
 import supertest from "supertest";
 
 describe("get posts", () => {
   it("should list all posts", async () => {
-    const res = await supertest(app()).get("/posts");
-    console.log(res);
+    const res = await supertest(app()).get("/posts").expect(200);
+
+    expect(res.body.data.length).not.toEqual(0);
   });
 
   it("should get a post", async () => {
-    const res = await supertest(app()).get("/posts/1");
-    console.log(res);
+    const res = await supertest(app()).get("/posts/20").expect(200);
+
+    expect(res.body.data).toMatchObject({
+      id: expect.any(Number),
+      text: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    });
+  });
+});
+
+describe("manage posts", () => {
+  it("should create a post", async () => {
+    const text = "booom";
+
+    await supertest(app())
+      .post("/posts")
+      .set("Content-type", "application/json")
+      .send({ text })
+      .expect(200);
+
+    const post = await Post.findOne({ where: { text } });
+
+    expect(post).not.toBeNull();
+    expect(post?.get).toHaveProperty("text", text);
   });
 });
